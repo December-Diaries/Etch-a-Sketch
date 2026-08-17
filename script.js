a63c0263c98bf962cece67ee51027c72ed522dbe
siva <sivaprakash.s4567@gmail.com>
2026-08-18 03:10:04 +0530

    create proper 16*16 grid that is viewable on screen with flexbox

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const container = document.querySelector("#container");
 
-for (let i=0; i<16;i++){
+for (let i=0; i<16*16;i++){
     console.log(`${i}`);
-    const div = document.createElement("div");
-    container.appendChild(div);
-    for(let j=0;j<15;j++){
-        const div2 = document.createElement("div");
-        div2.setAttribute("class","inline");
-        container.appendChild(div2);
-        console.log(`${j}`);
-    }
+    const div1 = document.createElement("div");
+    container.appendChild(div1);
+
 }
