131100a947a26a1804abb2127cd6f49f309b735b
siva <sivaprakash.s4567@gmail.com>
2026-08-18 03:41:17 +0530

    implement change color for hover

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const container = document.querySelector("#container");
 
+
 for (let i=0; i<16*16;i++){
     console.log(`${i}`);
     const div1 = document.createElement("div");
+    
+    div1.addEventListener("mouseenter",()=>{
+        div1.style.backgroundColor = "green";       
+    });
+    
     container.appendChild(div1);
 
+    
+
+
 }
+
+
+
