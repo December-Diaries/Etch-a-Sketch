6568f8be9d89ba56ecac90f4ae17bbbff4ba68c6
siva <sivaprakash.s4567@gmail.com>
2026-08-19 10:52:36 +0530

    change grid generation algo and add button for new grid

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const container = document.querySelector("#container");
 
+const btn = document.querySelector("button");
+
+
+btn.addEventListener("click",()=>{
+    let newGridSize = prompt("Enter number of squares per side (max 100)");
+
+});
 
 for (let i=0; i<16*16;i++){
     console.log(`${i}`);
     const div1 = document.createElement("div");
     
     div1.addEventListener("mouseenter",()=>{
-        div1.style.backgroundColor = "green";       
-    });
+        div1.style.backgroundColor = "green";
+
+    },{once: true});
     
     container.appendChild(div1);
-
-    
-
-
 }
+
+
+
+
+
 
 
 
