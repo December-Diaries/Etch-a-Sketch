8e8338b4dcbb7ca1cf9fb0c7ae0cca9ca6660147
siva <sivaprakash.s4567@gmail.com>
2026-08-19 13:08:24 +0530

    Implement new grid generation logic from user input for squares per side

--- script.js
+++ script.js
@@ -2,23 +2,35 @@ const container = document.querySelector("#container");
 
 const btn = document.querySelector("button");
 
+createGrid(16);
 
 btn.addEventListener("click",()=>{
-    let newGridSize = prompt("Enter number of squares per side (max 100)");
+    let newGridSize = Number(prompt("Enter number of squares per side (max 100)"));
+    if (newGridSize>100||newGridSize<1||!Number.isInteger(newGridSize)){
+        alert("Wrong input! Give again");
+        return;
+    }
+    container.replaceChildren();
+    container.style.setProperty('--grid-size',newGridSize);
+    createGrid(newGridSize);
 
 });
 
-for (let i=0; i<16*16;i++){
-    console.log(`${i}`);
-    const div1 = document.createElement("div");
-    
-    div1.addEventListener("mouseenter",()=>{
-        div1.style.backgroundColor = "green";
 
-    },{once: true});
-    
-    container.appendChild(div1);
+function createGrid(gridSize){
+    for (let i=0; i<gridSize*gridSize;i++){
+        
+        const div1 = document.createElement("div");
+        
+        div1.addEventListener("mouseenter",()=>{
+            div1.style.backgroundColor = "green";
+
+        },{once: true});
+        
+        container.appendChild(div1);
+    }
 }
+
 
 
 
