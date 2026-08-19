412b278782866d0d8ba9b0b3d31ba95f9c9133c4
siva <sivaprakash.s4567@gmail.com>
2026-08-19 17:02:52 +0530

    implement darkening effect through opacity increase

--- script.js
+++ script.js
@@ -5,27 +5,35 @@ const btn = document.querySelector("button");
 createGrid(16);
 
 btn.addEventListener("click",()=>{
-    let newGridSize = Number(prompt("Enter number of squares per side (max 100)"));
+    let input = prompt("Enter number of squares per side (max 100)");
+    if (input ==null) return;
+    let newGridSize =Number(input);
     if (newGridSize>100||newGridSize<1||!Number.isInteger(newGridSize)){
         alert("Wrong input! Give again");
         return;
     }
-    container.replaceChildren();
-    container.style.setProperty('--grid-size',newGridSize);
+
     createGrid(newGridSize);
 
 });
 
 
 function createGrid(gridSize){
+    container.replaceChildren();
+    container.style.setProperty('--grid-size',gridSize);
     for (let i=0; i<gridSize*gridSize;i++){
         
         const div1 = document.createElement("div");
-        
+        div1.dataset.alpha = "0";
         div1.addEventListener("mouseenter",()=>{
-            div1.style.backgroundColor = "green";
+            
+            
+            const currentOpacity =parseFloat(div1.dataset.alpha)||0;
+            let nextAlpha=Math.min(1,currentOpacity+0.1);
+            div1.dataset.alpha=nextAlpha;
+            div1.style.backgroundColor = `rgba(0, 128, 0, ${nextAlpha})`;
 
-        },{once: true});
+        });
         
         container.appendChild(div1);
     }
